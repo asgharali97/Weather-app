2da5e76c3753e486defc66fabdb5685c05964976
asgharali97 <asgharalifs48@gmail.com>
2024-05-20 23:24:42 -0700

    add some validation of searching and if response didn't come

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ const sunset = document.querySelector('.sunset')
 const wind_degrees = document.querySelector('.wind_degrees')
 const wind_speed = document.querySelector('.wind_speed')
 const icon = document.querySelector(".icon");
-const value = document.querySelector(".value");
+const input = document.querySelector(".value");
+const Message = document.querySelector(".Message");
+
 // const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city;
 const options = {
   method: "GET",
@@ -26,59 +28,71 @@ async function getWeather(city) {
       "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
       options
     );
+    if(!response.ok){
+      throw new error("Some error ouccar")
+    }
     const result = await response.json();
-    console.log(result);
+   if(result.error){
+    throw new error("Some error occared")
+   }
+      temp.innerHTML = result.temp + "°C";
+      humadity.innerHTML = result.humidity + "%";
+      feels.innerHTML = result.feels_like;
+      cloud_pct.innerHTML = result.cloud_pct;
+      max_temp.innerHTML = result.max_temp;
+      min_temp.innerHTML = result.min_temp;
+      sunrise.innerHTML = result.sunrise;
+      sunset.innerHTML = result.sunset;
+      wind_degrees.innerHTML = result.wind_degrees;
+      wind_speed.innerHTML = result.wind_speed;
+
+
     // displaying weather detail
-    temp.innerHTML = result.temp + "°C";
-    humadity.innerHTML = result.humidity + "%";
-    feels.innerHTML = result.feels_like;
-    cloud_pct.innerHTML = result.cloud_pct;
-    max_temp.innerHTML = result.max_temp;
-    min_temp.innerHTML = result.min_temp;
-    sunrise.innerHTML = result.sunrise;
-    sunset.innerHTML = result.sunset;
-    wind_degrees.innerHTML = result.wind_degrees;
-    wind_speed.innerHTML = result.wind_speed;
-  } catch (error) {
-    console.error(error);
+
+  } catch (error1) {
+    Message.innerHTML = "Sorry Please enter correct city name";
+    clearWeather()
+    setTimeout(() => {
+      Message.innerHTML = '';
+    }, 2000);
   }
+
 }
 getWeather("Karachi");
+
+// Weather Clearing Function
+function clearWeather (){
+  temp.innerHTML = "";
+  humadity.innerHTML = "";
+  feels.innerHTML = "";
+  cloud_pct.innerHTML = '';
+  max_temp.innerHTML ='';
+  min_temp.innerHTML = '';
+  sunrise.innerHTML = '';
+  sunset.innerHTML ='';
+  wind_degrees.innerHTML = '';
+  wind_speed.innerHTML =  '';
+}
 
 // hovering on icon to display search Bar
 
 icon.addEventListener("mouseover", function () {
   setTimeout(function () {
-    value.style.display = "block";
+    input.style.display = "block";
   }, 300);
 });
 
 // Search functionality
-value.addEventListener("keypress", function (e) {
+input.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     e.preventDefault();
-    // clearing previous value
-
-    temp.innerHTML = "";
-    humadity.innerHTML = "";
-    feels.innerHTML = "";
-    cloud_pct.innerHTML = '';
-    max_temp.innerHTML ='';
-    min_temp.innerHTML = '';
-    sunrise.innerHTML = '';
-    sunset.innerHTML ='';
-    wind_degrees.innerHTML = '';
-    wind_speed.innerHTML = 
-    // calling function with the city name
-    getWeather(value.value);
-
-    // clearing input value
-    value.value = "";
+    const city = input.value.trim();
+    if(city){
+      getWeather(city);
+    }
+    clearWeather();
+    input.value = "";
+    input.style.display = "none";
   }
 });
 
-value.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    value.style.display = "none";
-  }
-});
